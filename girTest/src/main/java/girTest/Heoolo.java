476156983f90b8d82ef1ddfe531916510714f906
SmallTak <1048388981@qq.com>
2018-03-20 19:09:36 +0800

    hebing

--- girTest/src/main/java/girTest/Heoolo.java
+++ girTest/src/main/java/girTest/Heoolo.java
@@ -4,14 +4,11 @@ public class Heoolo {
 
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
-		String name  = "tak";
-=======
+
 		int i = 10;
 		int a = i /10;
 		System.out.println(a);
 		
->>>>>>> dev
 		System.out.println("stay!");
 		
 	}
