31ab920ec93c541a0676a3c4001bdc5e86c2763d
SmallTak <1048388981@qq.com>
2018-03-20 19:05:56 +0800

    dev

--- girTest/src/main/java/girTest/Heoolo.java
+++ girTest/src/main/java/girTest/Heoolo.java
@@ -3,7 +3,11 @@ package girTest;
 public class Heoolo {
 
 	public static void main(String[] args) {
-
+		
+		int i = 10;
+		int a = i /10;
+		System.out.println(a);
+		
 		System.out.println("stay!");
 		
 	}
